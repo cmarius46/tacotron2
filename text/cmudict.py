957240149a33e2aa1f0a118732eeb8ed1665dedd
cmarius46 <cmarius46@gmail.com>
2023-05-05 00:37:45 +0300

    added new dict

--- text/cmudict.py
+++ text/cmudict.py
@@ -3,14 +3,20 @@
 import re
 
 
+# valid_symbols = [
+#   'AA', 'AA0', 'AA1', 'AA2', 'AE', 'AE0', 'AE1', 'AE2', 'AH', 'AH0', 'AH1', 'AH2',
+#   'AO', 'AO0', 'AO1', 'AO2', 'AW', 'AW0', 'AW1', 'AW2', 'AY', 'AY0', 'AY1', 'AY2',
+#   'B', 'CH', 'D', 'DH', 'EH', 'EH0', 'EH1', 'EH2', 'ER', 'ER0', 'ER1', 'ER2', 'EY',
+#   'EY0', 'EY1', 'EY2', 'F', 'G', 'HH', 'IH', 'IH0', 'IH1', 'IH2', 'IY', 'IY0', 'IY1',
+#   'IY2', 'JH', 'K', 'L', 'M', 'N', 'NG', 'OW', 'OW0', 'OW1', 'OW2', 'OY', 'OY0',
+#   'OY1', 'OY2', 'P', 'R', 'S', 'SH', 'T', 'TH', 'UH', 'UH0', 'UH1', 'UH2', 'UW',
+#   'UW0', 'UW1', 'UW2', 'V', 'W', 'Y', 'Z', 'ZH'
+# ]
+
 valid_symbols = [
-  'AA', 'AA0', 'AA1', 'AA2', 'AE', 'AE0', 'AE1', 'AE2', 'AH', 'AH0', 'AH1', 'AH2',
-  'AO', 'AO0', 'AO1', 'AO2', 'AW', 'AW0', 'AW1', 'AW2', 'AY', 'AY0', 'AY1', 'AY2',
-  'B', 'CH', 'D', 'DH', 'EH', 'EH0', 'EH1', 'EH2', 'ER', 'ER0', 'ER1', 'ER2', 'EY',
-  'EY0', 'EY1', 'EY2', 'F', 'G', 'HH', 'IH', 'IH0', 'IH1', 'IH2', 'IY', 'IY0', 'IY1',
-  'IY2', 'JH', 'K', 'L', 'M', 'N', 'NG', 'OW', 'OW0', 'OW1', 'OW2', 'OY', 'OY0',
-  'OY1', 'OY2', 'P', 'R', 'S', 'SH', 'T', 'TH', 'UH', 'UH0', 'UH1', 'UH2', 'UW',
-  'UW0', 'UW1', 'UW2', 'V', 'W', 'Y', 'Z', 'ZH'
+  'sh', 'ij', 'k', '@', 'zh', 'a', 'n', 'd', 'r', 'm', 'u', 'l', 'i', 't', 'a@', 'e', 'j', 
+  'h', 'o', 'dz', 'p', 'ch', 'g', 'v', 's', 'z', 'g.z', 'ts', '-', 'f', 'e@', 'k.s', 'w', 'b', 
+  'o@', 'j.e'
 ]
 
 _valid_symbol_set = set(valid_symbols)
@@ -58,6 +64,8 @@ def _parse_cmudict(file):
 
 
 def _get_pronunciation(s):
+  # verifies that s is comprised of sounds that are in _valid_symbol_set
+
   parts = s.strip().split(' ')
   for part in parts:
     if part not in _valid_symbol_set:
